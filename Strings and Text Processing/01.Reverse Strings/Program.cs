25aaa2275399870ac4db030f0e50bc2d9cec1a25
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-05 19:20:21 +0200

    Add files via upload

---
+++ Strings and Text Processing/01.Reverse Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+    string output = "";
+
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        output += input[i];
+    }
+
+    Console.WriteLine(input + " = " + output);
+
+    input = Console.ReadLine();
+}
---
+++ Strings and Text Processing/02.Repeat Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] words = Console.ReadLine().Split().ToArray();
+
+string result = "";
+
+foreach (string word in words)
+{
+    for (int i = 0; i < word.Length; i++)
+    {
+        result += word;
+    }
+}
+
+Console.WriteLine(result);
