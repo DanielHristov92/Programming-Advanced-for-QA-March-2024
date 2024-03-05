0404ee2ed313b4ae7124082f3f6670a8ca396d89
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-05 19:40:18 +0200

    Add files via upload

---
+++ Strings and Text Processing/03.Substring/Program.cs
@@ -0,0 +1,14 @@
+﻿string firstString = Console.ReadLine();
+
+string secondString = Console.ReadLine();
+
+while (secondString.Contains(firstString))
+{
+    int wordIndex = secondString.IndexOf(firstString);
+    string before = secondString.Substring(0, wordIndex);
+    string after = secondString.Substring(wordIndex + firstString.Length);  
+
+    secondString = before + after;
+}
+
+Console.WriteLine(secondString);
