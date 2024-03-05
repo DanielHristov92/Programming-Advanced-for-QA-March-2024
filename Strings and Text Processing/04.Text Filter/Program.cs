0d2ebfa7ddd9cd96cb5af6dce7f985b834a13e5e
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-05 19:58:17 +0200

    Add files via upload

---
+++ Strings and Text Processing/04.Text Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] bannedWords = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+string text = Console.ReadLine();
+
+foreach (string bannedWord in bannedWords)
+{
+    string asterisks = new string('*', bannedWord.Length);
+
+    text = text.Replace(bannedWord, asterisks);
+}
+
+Console.WriteLine(text);
