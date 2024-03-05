2a96adb43b2e335870f276e2bc82e03d54636a4b
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-05 20:44:00 +0200

    Add files via upload

---
+++ Strings and Text Processing/05.Digits,Letters and Other/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+
+StringBuilder digits = new StringBuilder();
+StringBuilder letters = new StringBuilder();
+StringBuilder others = new StringBuilder();
+
+foreach (char c in input)
+{
+    if (char.IsDigit(c))
+    {
+        digits.Append(c);
+    }
+    else if (char.IsLetter(c))
+    {
+        letters.Append(c);
+    }
+    else
+    {
+        others.Append(c);
+    }
+}
+
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(others);
