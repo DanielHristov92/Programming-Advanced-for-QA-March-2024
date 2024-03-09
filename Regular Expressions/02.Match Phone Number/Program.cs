4b0a07e1964801c1e15c84c4d9ee5c6391ce8141
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-09 09:17:20 +0200

    Add files via upload

---
+++ Regular Expressions/02.Match Phone Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"\+359([\ \-])2\1\d{3}\1\d{4}";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(input);
+
+List<string> validNumbers = new List<string>();
+
+foreach (Match match in matches)
+{
+    validNumbers.Add(match.Value);
+}
+
+Console.WriteLine(string.Join(", ", validNumbers));
---
+++ Regular Expressions/03.Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+string pattern = @"(?'day'\d{2})(?'del'[\/\.\-])(?'month'[A-Z][a-z]{2})\'del'(?'year'\d{4})";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(input);
+
+List<string> validDates = new List<string>();
+
+foreach (Match match in matches)
+{
+    string day = match.Groups["day"].Value;
+    string month = match.Groups["month"].Value;
+    string year = match.Groups["year"].Value;
+
+    validDates.Add($"Day: {day}, Month: {month}, Year: {year}");
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, validDates));
