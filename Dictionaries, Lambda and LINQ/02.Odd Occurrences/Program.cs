0cb47babdbecd0e3352f5e784741f8feb711d9e5
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-11 19:31:13 +0200

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ/02.Odd Occurrences/Program.cs
@@ -0,0 +1,28 @@
+﻿string input = Console.ReadLine();
+string[] words = input.Split(' ');
+
+Dictionary<string, int> wordCounts = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+foreach (string word in words)
+{
+    if (wordCounts.ContainsKey(word.ToLower()))
+    {
+        wordCounts[word.ToLower()]++;
+    }
+    else
+    {
+        wordCounts[word.ToLower()] = 1;
+    }
+}
+
+List<string> result = new List<string>();
+
+foreach (var pair in wordCounts)
+{
+    if (pair.Value % 2 != 0)
+    {
+        result.Add(pair.Key);
+    }
+}
+
+Console.WriteLine(string.Join(" ", result));
---
+++ Dictionaries, Lambda and LINQ/03.Word Synonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < n; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (!synonyms.ContainsKey(word))
+    {
+        synonyms[word] = new List<string>();
+    }
+
+    synonyms[word].Add(synonym);
+}
+
+foreach (var pair in synonyms)
+{
+    string word = pair.Key;
+    string synonymsList = string.Join(", ", pair.Value);
+    Console.WriteLine($"{word} - {synonymsList}");
+}
