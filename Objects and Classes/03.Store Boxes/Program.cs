e956a0358edd1a67b8c0833416e71b443a9508b1
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-18 19:33:44 +0200

    Add files via upload

---
+++ Objects and Classes/03.Store Boxes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Item
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
+
+public class Box
+{
+    public string SerialNumber { get; set; }
+    public Item Item { get; set; }
+    public int ItemQuantity { get; set; }
+    public double BoxPrice => ItemQuantity * Item.Price;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Box> boxes = new List<Box>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] data = input.Split();
+            string serialNumber = data[0];
+            string itemName = data[1];
+            int itemQuantity = int.Parse(data[2]);
+            double itemPrice = double.Parse(data[3]);
+
+            Item item = new Item { Name = itemName, Price = itemPrice };
+            Box box = new Box { SerialNumber = serialNumber, Item = item, ItemQuantity = itemQuantity };
+            boxes.Add(box);
+        }
+
+        foreach (Box box in boxes.OrderByDescending(b => b.BoxPrice))
+        {
+            Console.WriteLine($"{box.SerialNumber}");
+            Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:F2}: {box.ItemQuantity}");
+            Console.WriteLine($"-- ${box.BoxPrice:F2}");
+        }
+    }
+}
---
+++ Objects and Classes/04.Vehicle Catalogue/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+}
+
+public class Car : Vehicle
+{
+    public int HorsePower { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Brand}: {Model} - {HorsePower}hp";
+    }
+}
+
+public class Truck : Vehicle
+{
+    public int Weight { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Brand}: {Model} - {Weight}kg";
+    }
+}
+
+public class Catalog
+{
+    public List<Car> Cars { get; set; }
+    public List<Truck> Trucks { get; set; }
+
+    public Catalog()
+    {
+        Cars = new List<Car>();
+        Trucks = new List<Truck>();
+    }
+
+    public void AddVehicle(string type, string brand, string model, int horsePowerOrWeight)
+    {
+        if (type == "Car")
+        {
+            Cars.Add(new Car { Brand = brand, Model = model, HorsePower = horsePowerOrWeight });
+        }
+        else if (type == "Truck")
+        {
+            Trucks.Add(new Truck { Brand = brand, Model = model, Weight = horsePowerOrWeight });
+        }
+    }
+
+    public void PrintCatalog()
+    {
+        Console.WriteLine("Cars:");
+        foreach (var car in Cars.OrderBy(c => c.Brand))
+        {
+            Console.WriteLine(car);
+        }
+
+        Console.WriteLine("Trucks:");
+        foreach (var truck in Trucks.OrderBy(t => t.Brand))
+        {
+            Console.WriteLine(truck);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Catalog catalog = new Catalog();
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] vehicleData = input.Split('/');
+            string type = vehicleData[0];
+            string brand = vehicleData[1];
+            string model = vehicleData[2];
+            int horsePowerOrWeight = int.Parse(vehicleData[3]);
+
+            catalog.AddVehicle(type, brand, model, horsePowerOrWeight);
+        }
+
+        catalog.PrintCatalog();
+    }
+}
